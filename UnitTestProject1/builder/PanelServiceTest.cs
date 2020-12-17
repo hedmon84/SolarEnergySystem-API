13ee32465a2e0b96ea9702930ff032908dc9fe17
hedmon84 <hedmonlopes@gmail.com>
2020-12-17 00:10:54 -0600

    git

---
+++ UnitTestProject1/builder/PanelServiceTest.cs
@@ -0,0 +1,20 @@
+﻿//using Moq;
+//using SolarEnergySystem.API.Controllers;
+//using SolarEnergySystem.Core.Entities;
+//using SolarEnergySystem.Core.Interfaces;
+//using SolarEnergySystem.Core.Services;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace SolarEnergySystem.Tests.Builders
+//{
+//    public class PanelServiceTestBuilder
+//    {
+//       
+//        public PanelsController Build(IPanelService service)
+//        {
+//            return new PanelsController(service);
+//        }
+//    }
+//}
